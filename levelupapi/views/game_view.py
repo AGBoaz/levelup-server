90c01bb9cee09131f7def755a4e5d6cadd2752e0
Grace Boaz <anna.g.boaz@gmail.com>
2023-02-21 11:18:20 -0600

    the list of games will have sensable information, rather than a bunch of id numbers

--- levelupapi/views/game_view.py
+++ levelupapi/views/game_view.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from levelupapi.models import Game
+from levelupapi.models import Game, GameType, Gamer
 
 
 class GameView(ViewSet):
@@ -33,9 +33,25 @@ class GameView(ViewSet):
         return Response(serializer.data)
 
 
+class GameGamerSerializer(serializers.ModelSerializer):
+    """ to serialize the gamer in GamerSerializer """
+    class Meta:
+        model = Gamer
+        fields = ('id', 'bio', 'user_id')
+
+class GameTypeSerializer(serializers.ModelSerializer):
+    """ to serialize the gamer in GamerSerializer """
+    class Meta:
+        model = GameType
+        fields = ('id', 'type')
+
 class GameSerializer(serializers.ModelSerializer):
     """JSON serializer for games
     """
+    gamer = GameGamerSerializer(many=False)
+    game_type = GameTypeSerializer(many=False)
+
     class Meta:
         model = Game
         fields = ('id', 'name', 'description', 'game_type', 'gamer')
+        depth: 1
